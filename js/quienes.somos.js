3c9fa3bebb485eb97a0688620d95d10564cd85ee
Maite Albozta <mai-analia@hotmail.com>
2023-01-15 13:32:21 -0300

    localstorage

--- js/quienes.somos.js
+++ js/quienes.somos.js
@@ -22,15 +22,15 @@ btnIngreso.addEventListener ("click", function () {
 
 //guardar datos usuario y contraseña
 
-let user = document.getElementById("usuario"); 
-let pass= document.getElementById("contraseña");
+let user = document.getElementById("exampleInputUsuario1"); 
+let pass= document.getElementById("exampleInputPassword1");
 let btn = document.getElementById ("ingreso"); 
 
 btn.addEventListener("click", () => {
     console.log("Registrando usuario")
    // acá hacés los dos setItem.
-   localStorage.setItem ("usuario1", JSON.stringify(usuario.value));
-   localStorage.setItem ("contraseña1", JSON.stringify (contraseña.value));
+   localStorage.setItem ("usuario1", JSON.stringify(user.value));
+   localStorage.setItem ("contraseña1", JSON.stringify (pass.value));
 })
 
 
@@ -42,33 +42,6 @@ btn.addEventListener("click", () => {
 
 
 
-/* function set() {
-    let usuarioJson=JSON.stringify (usuario)
-    let contraseñaJson=JSON.stringify (contraseña)
-}
-
-set (localStorage.setItem);
-
-let examplePassword= document.getElementById(exampleInputPassword1);
-let exampleUsuario = document.getElementById(exampleInputUsuario1);
-
-//almacena objeto opciones productos en el local storage
-localStorage.getItem("usuario", exampleUsuario)
-localStorage.getItem ("contraseña", examplePassword)
 
 
-console.log(exampleInputUsuario1, exampleInputPassword1);
- */
 
-/* 
-
-
-//recuperando el objeto opciones productos del local storage
-let datoUsuario=localStorage.getItem("usuario");
-let datoContraseña=localStorage.getItem("contraseña");
-
-
-    
-let user = document.querySelector(".usuario.value")
-    
-let pass = document.querySelector(".contraseña.value")  */
