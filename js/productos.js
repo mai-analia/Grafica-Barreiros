fbbe18b735546fd6f10ccb6cff55ae807073d93f
Maite Albozta <mai-analia@hotmail.com>
2023-01-28 00:14:58 -0300

    ruta 7

--- js/productos.js
+++ js/productos.js
@@ -151,6 +151,27 @@ compra.addEventListener ("click", () => {
 
 
 
+//AJAX Y FETCH
+//await
+const lista = document.querySelector("#listado");
+
+const pedirProductos = async() => {
+    const resp= await fetch("../js/data.json"); //pensar la peticion como si estuvieramos en el html
+    const data=await resp.json ();
+
+    data.forEach ((productos) =>{
+        const li =document.createElement("li")
+        li.innerHTML= `
+        <h4>${productos.producto}</h4>
+        <h7> $ ${productos.precio}</h7>
+        <p>${productos.medidas}</p>
+        `
+        lista.append(li)
+    });
+};
+
+pedirProductos();
+
 
 
 
